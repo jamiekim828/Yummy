c84f844ffb602d3fb973abd6c77784df041662d6
jamiekim828 <jamiekim828@gmail.com>
2022-12-15 10:58:58 +0100

    add contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from 'react';
 
 export default function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className='contact' style={{ textAlign: 'center' }}>
+      <h1>Contact Us</h1>
+      <p>
+        We love hearing from you! Please contact us via the email address or
+        phone numbers below.
+      </p>
+      <p>
+        For answers to frequently asked questions about your subscription,
+        please check out ourFAQ page
+      </p>
+      <p>
+        <strong>Email address:</strong>help@yummy.com
+      </p>
+      <p>We will respond to your email within 72 hours.</p>
+      <p>
+        <strong>Toll-free number:</strong>855-753-1037
+      </p>
+      <p>
+        <strong>International number:</strong>332-239-2921
+      </p>
+      <p>Hours of operation: Monday through Friday. 9 a.m. to 7 p.m. E.T.</p>
+    </div>
+  );
 }
